b632bb2deed0a9df3335a5e471f602032f395b80
Fabulousman23 <andrew.makiev@gmail.com>
2022-01-15 18:43:10 -0500

    to do list ready

---
+++ script.js
@@ -0,0 +1,41 @@
+// 1. Adding a new item to the list of items:
+
+function newItem() {
+    let list = $('#list');
+    let li = $('<li></li>');
+    let input = $('#input');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
+
+    if (inputValue === ' ') {
+        alert('You must write smth!');
+    } else {
+        $('#list').append(li);
+        $('# input').val();
+    }
+// 2. crosing out:
+    function crossOut() {
+        li.toggleClass('strike');
+        
+    }
+
+
+li.on('dblclick', crossOut);
+
+// 3. add delete button
+
+let crossOutButton = $('<crossOutButton></crossOutButton>')
+    crossOutButton.append(document.createTextNode('X'));
+    li.append(crossOutButton);
+
+
+function deleteListItem() {
+        li.addClass('delete');
+    }
+    crossOutButton.on('click', deleteListItem);
+
+// 4. make list sortable
+
+$('#list').sortable()
+
+}
